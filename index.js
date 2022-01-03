795c8c45a6e4dc4b85b727fb3a45f741bece6216
pietroconta <42576699+pietroconta@users.noreply.github.com>
2022-01-03 14:57:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 (async () => {
   //const browser = await puppeteer.launch({headless:false});
-  const browser = await puppeteer.launch({headless:false
+ const browser = await puppeteer.launch({
+  executablePath: '/usr/bin/chromium-browser'
 })
   const page = await browser.newPage();
   await page.waitForTimeout(1000);     //callibrate as per your need
